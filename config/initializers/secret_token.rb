2b9e79cf64274340220ba16d202ff376ad6dba33
Paul Wittmann <paul@railslove.com>
2012-04-08 15:24:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfcynGallery::Application.config.secret_token = '4726d8183508dde38e96741831cddb7ae6565426f9fad748ff238afa7afafe243e3b40d580def3408b951ee49afbda1ba49151f712b0bb5b9da0ab38363904c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProfcynGallery::Application.config.session_store :cookie_store, key: '_profcyn-gallery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProfcynGallery::Application.config.session_store :active_record_store
