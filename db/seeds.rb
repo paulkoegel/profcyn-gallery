e6d64c839d5f3c9f91115a4d54f2e029f3e1e35a
Paul Wittmann <paul@railslove.com>
2012-04-23 20:41:42 +0200

    Seeds: create Images from assets/images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
+require 'dimensions'
 
+file_names = Dir['app/assets/images/*/**'].delete_if{|file_name| file_name !~ /\.(png|jpg|jpeg|gif)$/}
+file_names.each do |file_name|
+  Image.create(
+    :path   => file_name.gsub('app/assets/images/', ''),
+    :width  => Dimensions.width(file_name),
+    :height => Dimensions.height(file_name)
+  )
+end
