ed64dd7daabf2a069629beb96932c4af4ad677cf
Paul Wittmann <paul@railslove.com>
2012-04-24 01:33:16 +0200

    upload multiple files at once (plan HTML5, no jQuery magic)

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -17,8 +17,10 @@ class ImagesController < ApplicationController
 
   def create
     # POST params:
-    # {"utf8"=>"✓", "authenticity_token"=>"BpXiqpB5mgud3jb73MBPw5jeiVA3qvVTg7a6Z7DLflg=", "image"=>{"file"=>#<ActionDispatch::Http::UploadedFile:0x007ff35c61c180 @original_filename="Bud Bad.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"image[file]\"; filename=\"Bud Bad.jpg\"\r\nContent-Type: image/jpeg\r\n", @tempfile=#<File:/var/folders/tj/7gyp9lps2c163wl9nmwt0jrc0000gn/T/RackMultipart20120424-14324-d6x4rr>>}, "commit"=>"Create Image"}
-    @client.upload params[:image][:file].original_filename, params[:image][:file].tempfile
+    # {"utf8"=>"✓", "authenticity_token"=>"MPcV4fAqshmx2/WcJO4FWah92Mp9kpWcrEdPGao8rSc=", "image"=>{"file"=>[#<ActionDispatch::Http::UploadedFile:0x007f9bd3313578 @original_filename="Frank424766_10150505770452687_668482686_9274392_249914352_n.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"image[file][]\"; filename=\"Frank424766_10150505770452687_668482686_9274392_249914352_n.jpg\"\r\nContent-Type: image/jpeg\r\n", @tempfile=#<File:/var/folders/tj/7gyp9lps2c163wl9nmwt0jrc0000gn/T/RackMultipart20120424-14985-8qhgcr>>, #<ActionDispatch::Http::UploadedFile:0x007f9bd33137d0 @original_filename="Fukushima-3D79.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"image[file][]\"; filename=\"Fukushima-3D79.jpg\"\r\nContent-Type: image/jpeg\r\n", @tempfile=#<File:/var/folders/tj/7gyp9lps2c163wl9nmwt0jrc0000gn/T/RackMultipart20120424-14985-4reozb>>]}, "commit"=>"Create Image"}
+    params[:image][:files].each do |file|
+      @client.upload file.original_filename, file.tempfile
+    end
   end
 
   def new
--- lib/tasks/images.rake
+++ lib/tasks/images.rake
@@ -5,7 +5,7 @@ namespace :images do
   desc 'Query Dropbox API for image paths & store them on the DB.'
   task :update_paths => :environment do
     client = Dropbox::API::Client.new(:token => Settings.dropbox.token, :secret => Settings.dropbox.secret)
-    puts client.ls[0].ls.map(&:direct_url)
+    #puts client.ls[0].ls.map(&:direct_url)
   end
 
 end
